a8712028b86f1b1101d39027f1d3756dd0b26883
silvercoin <silvercoin99.999@gmail.com>
2021-04-11 00:04:23 +0900

    회원가입

---
+++ USER/asset/js/sub.js
@@ -0,0 +1,45 @@
+// * -------------------------------
+// * all-check-btn(checkbox)
+// * -------------------------------
+$(document).on("change", "input[type=checkbox].all-check-btn", function () {
+  var check_match = $(this).data("checkMatch");
+
+  if ($(this).is(":checked")) {
+    $("input[type=checkbox]").each(function () {
+      if ($(this).data("checkMatch") == check_match) {
+        $(this).prop("checked", true);
+      }
+    });
+  } else {
+    $("input[type=checkbox]").each(function () {
+      if ($(this).data("checkMatch") == check_match) {
+        $(this).prop("checked", false);
+      }
+    });
+  }
+});
+
+$(document).on(
+  "change",
+  'input[type=checkbox]:not(".all-check-btn")',
+  function () {
+    var check_match = $(this).data("checkMatch");
+    var ctn = 0;
+
+    $('input[type=checkbox]:not(".all-check-btn")').each(function () {
+      if ($(this).data("checkMatch") == check_match) {
+        if (!$(this).is(":checked")) {
+          ctn++;
+        }
+      }
+    });
+
+    $("input[type=checkbox].all-check-btn").each(function () {
+      if ($(this).data("checkMatch") == check_match) {
+        ctn > 0
+          ? $(this).prop("checked", false)
+          : $(this).prop("checked", true);
+      }
+    });
+  }
+);
