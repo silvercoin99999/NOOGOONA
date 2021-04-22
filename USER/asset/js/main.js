c5d097e8120ff674e368f1880beb9cc7dfa08b5b
silvercoin <silvercoin99.999@gmail.com>
2021-04-22 09:09:27 +0900

    세이브커밋

--- USER/asset/js/main.js
+++ USER/asset/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-	var swiper = new Swiper('.swiper-container', {
-		slidesPerView: 4,
-		spaceBetween: 10,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
+$(document).ready(function () {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
